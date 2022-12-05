1b5db134030518b1110805cc585e96b11767e222
Muhammad Awais <91396981+awaiskhalid92@users.noreply.github.com>
2022-12-05 20:33:16 +0500

    layout Changes

--- Code/client/src/components/home/InfoCard.jsx
+++ Code/client/src/components/home/InfoCard.jsx
@@ -13,20 +13,20 @@ import {
 
 export default function InfoCard(props) {
   return (
-    <div className=" h-25 d-incline-block mt-1">
-      <MDBCard className=" w-100 mx-auto p-1 bg-danger rounded-0">
+    <div className="d-incline-block mt-1">
+      <MDBCard className=" w-100  mx-auto p-1 bg-dark rounded-0">
         <MDBRow className="g-0">
-          <MDBCol md="4">
+          <MDBCol md="3">
             <MDBCardImage
               src="https://images.pexels.com/photos/10963373/pexels-photo-10963373.jpeg?auto=compress&cs=tinysrgb&w=800"
               alt="..."
               fluid
             />
           </MDBCol>
-          <MDBCol md="8">
-            <MDBCardBody>
-              <MDBCardTitle>{props.aboutus}</MDBCardTitle>
-              <MDBCardText>
+          <MDBCol md="9">
+            <MDBCardBody className="text-light">
+              <MDBCardTitle className="text-light">{props.aboutus}</MDBCardTitle>
+              <MDBCardText >
                 Description Lorem ipsum dolor, sit amet consectetur adipisicing
                 elit. Dignissimos amet, dolore architecto aspernatur mollitia
                 velit vero nobis tempora, explicabo consequatur eum quis! Dolor.
@@ -45,9 +45,9 @@ export default function InfoCard(props) {
                   Shop Now{" "}
                 </Link>{" "}
               </MDBBtn>
-              <MDBBtn className="mx-1 ml-5" color="dark">
+              <MDBBtn className="mx-1 ml-5" color="light">
                 {" "}
-                Explore{" "}
+                Sell Now{" "}
               </MDBBtn>
             </MDBCardBody>
           </MDBCol>
